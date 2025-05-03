f326e80cb2cdc618c2a2b64ad4803d25f33f4a71
patomon002 <patrickomorovan@gmail.com>
2025-05-03 21:50:14 +0100

    add

--- Rock-Paper.py
+++ Rock-Paper.py
@@ -1,10 +1,23 @@
 
 import random
 
-computer = random.choices('Rock')
+computer = ('Rock', 'Paper', 'Scissors')
+
+computer_choice = random.choice(computer)
 
 me = input('Enter either Rock, Paper or Scissors: \n')
-if computer == me:
+if computer_choice == me:
     print('It is a tie')
+elif computer_choice == 'Rock' and me == 'Scissors':
+    print('Computer Wins')
+elif computer_choice == 'Rock' and me == 'Paper':
+    print('Computer Wins')
+elif computer_choice == 'Paper' and me == 'Rock':
+    print('You win')
+elif computer_choice == 'Scissors' and me == 'Rock':
+    print('You win')
+else:
+    print("Computer choice was " + computer_choice + " and this condition was not defined")
+
 
 
